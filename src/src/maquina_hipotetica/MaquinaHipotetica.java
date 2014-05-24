90669f817e5d373580332c7b474d7c5af3951c7a
jorge lucas <jorgelucasl91@gmail.com>
2014-05-24 11:22:13 -0300

    menuInteratico
    implementação do metodo menuInterativo

--- src/src/maquina_hipotetica/MaquinaHipotetica.java
+++ src/src/maquina_hipotetica/MaquinaHipotetica.java
@@ -1,22 +1,24 @@
-package maquina_hipotetica;
+﻿package maquinaHipotetica;
 
-import com.sun.corba.se.spi.legacy.connection.GetEndPointInfoAgainException;
+import java.util.Scanner;
+
+
 
 public class MaquinaHipotetica 
 {
 
 	// Contador de programa, registrador que retem
-	// o endereço da próxima instrução.
+	// o endereÃ§o da prÃ³xima instruÃ§Ã£o.
 	String[] pc = new String[2];
 	
-	//Registrador de instrução, retem a instrução
-	// atual, em execução.
+	//Registrador de instruÃ§Ã£o, retem a instruÃ§Ã£o
+	// atual, em execuÃ§Ã£o.
 	String[] ri = new String[4];
 	
 	// Banco de registradores.
 	String[][] reg = new String[16][2];
 	
-	// Banco de memória principal.
+	// Banco de memÃ³ria principal.
 	String[][] mp = new String[256][2];
 	
 	public static int getEndMemoria(String end)
@@ -28,8 +30,41 @@ public class MaquinaHipotetica
 	public void promptInterativo()
 	{
 		int pi_estado = 0;
+		
+		Scanner entrada = new Scanner(System.in);
+		// String para exibir as intruções digitas
+		String exibe = "";
+		// string para testar se a intrução é a instrução de parada
+		String testa = "";
+		
+		
+		System.out.println("digite a instrução");
 		while(true)
 		{
+			testa= entrada.next() ;
+			
+			exibe += testa + "\n";
+			
+			// exibe as instruções que ja foram digitadas
+			System.out.println(exibe.toUpperCase());
+			
+			
+			// testa se a instrução é @
+		if(testa.toUpperCase().substring(0, 1).equals("@")){
+			pi_estado = 3;
+				
+			}
+		// testa se a instrução é #
+		else if (testa.toUpperCase().substring(0,1).equals("#"))
+		{
+			pi_estado = 1;
+			
+		}
+		else if (testa.toUpperCase().substring(0,1).equals(" "))
+				{
+			
+				}
+			
 			
 		}
 	}
