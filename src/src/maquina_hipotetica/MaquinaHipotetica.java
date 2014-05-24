dd542e95f757544b5ffbd947a32e0a9c3a0c2142
sousamaf <marco.volare@gmail.com>
2014-05-24 14:26:32 -0300

    no
    no

--- src/src/maquina_hipotetica/MaquinaHipotetica.java
+++ src/src/maquina_hipotetica/MaquinaHipotetica.java
@@ -1,4 +1,4 @@
-﻿package maquinaHipotetica;
+﻿package maquina_hipotetica;
 
 import java.util.Scanner;
 
@@ -8,17 +8,17 @@ public class MaquinaHipotetica
 {
 
 	// Contador de programa, registrador que retem
-	// o endereÃ§o da prÃ³xima instruÃ§Ã£o.
+	// o endereço da próxima instrução.
 	String[] pc = new String[2];
 	
-	//Registrador de instruÃ§Ã£o, retem a instruÃ§Ã£o
-	// atual, em execuÃ§Ã£o.
+	//Registrador de instrução, retem a instrução
+	// atual, em execução.
 	String[] ri = new String[4];
 	
 	// Banco de registradores.
 	String[][] reg = new String[16][2];
 	
-	// Banco de memÃ³ria principal.
+	// Banco de memória principal.
 	String[][] mp = new String[256][2];
 	
 	public static int getEndMemoria(String end)
@@ -37,37 +37,37 @@ public class MaquinaHipotetica
 		// string para testar se a intrução é a instrução de parada
 		String testa = "";
 		
-		
-		System.out.println("digite a instrução");
 		while(true)
 		{
-			testa= entrada.next() ;
-			
-			exibe += testa + "\n";
 			
 			// exibe as instruções que ja foram digitadas
 			System.out.println(exibe.toUpperCase());
-			
+
+			System.out.print(">>> ");
+			testa = entrada.next() ;
+			exibe += testa + "\n";
+						
 			
 			// testa se a instrução é @
-		if(testa.toUpperCase().substring(0, 1).equals("@")){
-			pi_estado = 3;
+			if(testa.toUpperCase().substring(0, 1).equals("@"))
+			{
+				pi_estado = 3;
+					
+			}
+			// testa se a instrução é #
+			else if (testa.toUpperCase().substring(0,1).equals("#"))
+			{
+				pi_estado = 1;
 				
 			}
-		// testa se a instrução é #
-		else if (testa.toUpperCase().substring(0,1).equals("#"))
-		{
-			pi_estado = 1;
-			
-		}
-		else if (testa.toUpperCase().substring(0,1).equals(" "))
-				{
+			else if (testa.toUpperCase().substring(0,1).equals(" "))
+			{
 
-			
-				}
-			
-			
-		}
+		
+			}
+				
+				
+			}
 	}
 	
 	public static void main(String[] args)
