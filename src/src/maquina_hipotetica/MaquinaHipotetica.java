a6bd38ddfa9ee6cb90a96f546c36facb577ddbca
sousamaf <marco.volare@gmail.com>
2014-05-24 11:02:33 -0300

    Bancos de memória e registradores
    Arquivo inicial da implementação da máquina hipotética. Representação
    da memória e registradores.

---
+++ src/src/maquina_hipotetica/MaquinaHipotetica.java
@@ -0,0 +1,34 @@
+package maquina_hipotetica;
+
+import com.sun.corba.se.spi.legacy.connection.GetEndPointInfoAgainException;
+
+public class MaquinaHipotetica 
+{
+
+	// Contador de programa, registrador que retem
+	// o endereço da próxima instrução.
+	String[] pc = new String[2];
+	
+	//Registrador de instrução, retem a instrução
+	// atual, em execução.
+	String[] ri = new String[4];
+	
+	// Banco de registradores.
+	String[][] reg = new String[16][2];
+	
+	// Banco de memória principal.
+	String[][] mp = new String[256][2];
+	
+	public static int getEndMemoria(String end)
+	{
+		int pos = Integer.parseInt(end, 16);
+		return pos;
+	}
+	
+	//public 
+	
+	public static void main(String[] args)
+	{
+		System.out.println(getEndMemoria("56"));
+	}
+}
